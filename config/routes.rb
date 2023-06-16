84919eccc622f838cabba84c447d2debe2883739
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-06-16 13:29:50 +0000

    Added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root "photos#index"
-
-  
-
   
   devise_for :users
   
@@ -11,5 +8,5 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: :show
+  get "/:username" => "users#show"
 end
